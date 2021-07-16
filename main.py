b400ba59fdb5ccbefafbfe407cdbffe064ff68ff
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-07-16 19:25:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+num=12
+count=1
+while count<11:
+    print(num,"X",count,"=",num*count)
+    count=count+1
+
+
+    #another method
+num=14
+for count in range(1,11):
+     print(num,"X",count,"=",num*count)
